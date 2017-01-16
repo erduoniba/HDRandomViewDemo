1ff955741cfdd3e04685b3929a72b196d4fa5779
denglibing <denglibing@fangdd.com>
2017-01-16 12:44:06 +0800

    Initial Commit

---
+++ HDRandomViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HDRandomViewDemo
+//
+//  Created by denglibing on 2017/1/16.
+//  Copyright © 2017年 denglibing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HDRandomViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HDRandomViewDemo
+//
+//  Created by denglibing on 2017/1/16.
+//  Copyright © 2017年 denglibing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
